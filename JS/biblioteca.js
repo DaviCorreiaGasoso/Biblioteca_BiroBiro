7e5b6faa41dd80b7c8250736ccbeb031fbd39eaa
DaviCorreiaGasoso <davicorreiao07@gmail.com>
2024-09-25 15:10:39 -0300

    Primeira versão do JS da Biblioteca

--- JS/biblioteca.js
+++ JS/biblioteca.js
@@ -1,3 +1,25 @@
-function adicionarLivro {
-    let
+class Livro  {
+    constructor(titulo, autor, anoPublicacao, genero) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.genero = genero;
+    };
+};
+
+class Biblioteca {
+    adicionarLivro(livro){
+        this.biblioteca = [];
+        this.biblioteca.push(livro);
+    };
+
+    listarLivro(){
+        return this.biblioteca;
+    };
+
+    excluirLivro(livro){
+        encontrar_livro = livro.titulo
+        indice = this.biblioteca.findIndex(livro => livro.titulo === encontrar_livro);
+        this.biblioteca.splice(0, indice)
+    }
 };
